d89ffd2a5bd82fbe2ed0fb9b30babb16468dc10e
Ashraff93 <ruhaizanfazreen@gail.com>
2023-02-27 00:45:55 +0800

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+
 # NanoRadar_NR15
-NanoRadar NR15
+Basic calling for NanoRadar_NR15
+
+These drivers use Uart to communicate, 4 pins are required to interface.
+...
+  Nr15.IncomingNewMessage(); //Call IncomingNewMessage From Front Sensor
+  Serial.println(Nr15.Distance_Uart_s25); //put soe delay
+...
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <Arduino.h> //Declared weak in Arduino.h to allow user redefinitions.
 #include "NRA15.h" //#include "NRA15.h" 
-#define NR15 Serial1 //#define TELEMSERIAL Serial1 RX:TX(0,1)
+#define NR15 Serial1 //#define NR15 Serial1 RX:TX(0,1)
 unsigned long PORT_SENSOR_TIMEOUT_MS = 2000; //Time millisecond before turn off sensor
 
 
-NRA15 Nr15(NR15); //Define UART3SERIAL to class NRA15 with LedSS3
-uint8_t id25 = 25; /*<  Onboard ID of the sensor*/
-uint8_t orientation25 = 25; //Direction the sensor faces Sector 25 (Downward)
+NRA15 Nr15(NR15); //Define NRA15 to class NRA15 
+
 void setup() {
   delay(5000); //Delay 5 second before procced to the next line
   Serial.begin(115200); // opens serial port, sets data rate to 115200 bps
